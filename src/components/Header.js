174a1dcedcb032431f2b3018b126ef33bb41b4af
Max <hello@maxking.com.au>
2021-09-08 20:33:29 +1000

    Change format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { style } from '../assets/style/components/header.scss';
-import  heroImage from '../assets/img/max.jpg';
+import heroImage from '../assets/img/max.jpg';
 
 const Header = () => {
     return (
         <div>
-            <div className="text-white bg-black sticky top-0 right-0 left-0 border-2 border-red-500">
-                s
+            <div className="text-white bg-black sticky top-0 right-0 left-0 grid grid-cols-2 p-5">
+                <div className="grid justify-start">1</div>
+                <div className="grid justify-end">1</div>
             </div>
-        <div className="bg-black font-semibold grid grid-cols-2 mx-headerCont">
-            
-            <div className="text-white grid items-center mx-headerCont__left">
-                <div className="pl-32 mx-headerCont__left__herowrapper">
-                    <div className="leading-none mx-headerCont__left__herowrapper__hero">
-                        <div>Hello,</div>
-                        <div>I'm</div>
-                        <div className="mx-headerCont__primary">Max</div>
+            <div className="bg-black font-semibold grid grid-cols-2 mx-headerCont">
+                <div className="text-white grid items-center mx-headerCont__left">
+                    <div className="pl-32 mx-headerCont__left__herowrapper">
+                        <div className="leading-none mx-headerCont__left__herowrapper__hero">
+                            <div>Hello,</div>
+                            <div>I'm</div>
+                            <div className="mx-headerCont__primary">Max</div>
+                        </div>
+                        <div className="text-gray-600 font-light mx-headerCont__left__herowrapper__sub">
+                            <div>Front-End Developer / UI Designer</div>
+                        </div>
                     </div>
-                    <div className="text-2xl font-light mx-headerCont__left__herowrapper__sub">
-                        Front-End Developer
+                </div>
+
+                <div className="text-white grid items-center justify-items-center">
+                    <div className="w-3/4">
+                        <img className="w-full" alt="Max Attila King" src={heroImage} />
                     </div>
                 </div>
             </div>
-
-            <div className="text-white grid items-center justify-items-center">
-                    <div className="w-3/4">
-                       <img className="w-full" alt="Max Attila King" src={heroImage}/>
-                    </div>
-            </div>
         </div>
-        </div>
-
     )
-
 }
 
 export default Header;
