eac4038c21a3a2003567deac8a8d5dc99450fe05
Max <hello@maxking.com.au>
2021-09-11 21:47:59 +1000

    Add menuitems

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const SideNav = (props) => {
     return (
-        <div className={`fixed bottom-0 top-0 w-0 hover:text-red-500 transition-width duration-500 ease-in-out bg-black overflow-hidden ${props.Open ? "w-64" : ""}`}>
-            rerer
+        <div className={`grid grid-cols-1 gap-6 pl-10 text-white text-3xl font-light fixed bottom-0 top-0 w-0 pt-20 transition-width duration-500 ease-in-out bg-black overflow-hidden ${props.Open ? "w-64" : "w-0"}`}>
+            <div className="whitespace-no-wrap">
+                Home
+            </div>
+            <div className="whitespace-no-wrap">
+                About Me
+            </div>
+            <div className="whitespace-no-wrap">
+                My Work
+            </div>
         </div>
     )
 }
--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -16,7 +16,7 @@ const Topnav = () => {
         <>
             <div className="text-white bg-black sticky top-0 right-0 left-0 grid grid-cols-2 pl-6 pr-6 pt-3 pb-3 z-40">
                 <div className="grid justify-start items-center cursor-pointer pl-4" onClick={onSideMenuClicked}>
-                    <img class="opacity-75 transform rotate-0 hover:rotate-180 transition-rotate duration-500 ease-in-out" style={{ "width": "30px" }} alt="github" src={isSideMenuOpen ? cancelImg : burgerImg} />
+                    <img class="opacity-75 transform rotate-0 hover:rotate-180 transition-rotate duration-500 ease-in-out" style={{ "width": isSideMenuOpen ? "25px" : "30px" }} alt="menu" src={isSideMenuOpen ? cancelImg : burgerImg} />
                 </div>
                 <div className="grid justify-end items-center">
                     <div class="hint hint--left" aria-label="Repo of this website">
